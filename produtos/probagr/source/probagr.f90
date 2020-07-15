c6f7b24c15ab4345b7c532410f6eabc43b01cc5c
Carlos Frederico Bastarz <cfbastarz@gmail.com>
2020-07-15 17:03:14 +0000

    Tarefa #7839: idem commit anterior, removendo a excessão da rotina probagr.f90.

--- produtos/probagr/source/probagr.f90
+++ produtos/probagr/source/probagr.f90
@@ -145,9 +145,7 @@
         DO nmb=1,nmembers
 	   statfctrd=0
 	   CALL ReadGrib(TRIM(dirin)//TRIM(lstingrb(nmb,nbs)),imax,jmax,prec,statfctrd)
-!CFB
-!	   IF (statfctrd .NE. 0) STOP 'Main Program: It does not read grib correctly'
-!CFB
+	   IF (statfctrd .NE. 0) STOP 'Main Program: It does not read grib correctly'
            prcwkac(1:imax,1:jmax,nmb) = prcwkac(1:imax,1:jmax,nmb) + (prec(1:imax,1:jmax)/FLOAT(noutpday)) 
         ENDDO
 
