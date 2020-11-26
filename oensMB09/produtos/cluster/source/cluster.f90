f8595af5982bc16a107a4982fa7ff028f009ab01
Carlos Frederico Bastarz <cfbastarz@gmail.com>
2020-11-26 21:44:13 +0000

    Tarefa #7839: Adicionando arquivo source/Makefile.gnu e corrigindo instrução write na rotina source/cluster.f90.

--- oensMB09/produtos/cluster/source/cluster.f90
+++ oensMB09/produtos/cluster/source/cluster.f90
@@ -977,8 +977,11 @@ END PROGRAM cluster
      WRITE(datep,'(I4.4,3I2.2)') iyy,imm,idd,ihh
      fctdate(i)(1:10)=datep(1:10)
 
-     WRITE(*,'(A,I,2A)') 'fctdate(',i,'):',fctdate(i)(1:10)
-     
+!CFB
+!     WRITE(*,'(A,I,2A)') 'fctdate(',i,'):',fctdate(i)(1:10)
+     WRITE(*,'(A,I3,2A)') 'fctdate(',i,'):',fctdate(i)(1:10)
+!CFB    
+ 
   END DO
   
   RETURN
